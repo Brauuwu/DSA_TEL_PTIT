e9ffc1cee80d64959222e9d23379b61d4b4e5410
Brauuwu <survivalminecraft080@gmail.com>
2025-03-04 17:43:37 +0700

    DFS TRÊN ĐỒ THỊ VÔ HƯỚNG

---
+++ CON9_06.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+vector<int> Adj[105];
+int visited[105];
+
+void DFS(int u){
+	visited[u] = 1;
+	cout << u << " ";
+	for(int v:Adj[u]){
+		if(!visited[v]) DFS(v);
+	}
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+    	memset(Adj, 0, sizeof(Adj));
+        memset(visited, 0, sizeof(visited));
+    	int V, E, u;
+		cin >> V >> E >> u;
+		while(E--){
+			int x, y; cin >> x >> y;
+			Adj[x].push_back(y);
+			Adj[y].push_back(x);
+		}
+		DFS(u);
+		cout << endl;
+    }
+    return 0;
+}
