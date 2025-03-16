a551fd064a6cb8d1c7a4021d9051892389ff9c2a
Brauuwu <survivalminecraft080@gmail.com>
2025-03-16 13:13:51 +0700

    DÃY NGOẶC ĐÚNG DÀI NHẤT

---
+++ CON7_05.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+        string s; cin >> s;
+        int res = 0;
+        stack<int> st;
+        st.push(-1);
+        for(int i=0; i<s.size(); i++){
+        	if(s[i] == '(') st.push(i);
+        	else{
+        		st.pop();
+        		if(!st.empty()) res = max(res, i-st.top());
+        		else st.push(i);
+			}
+		}
+		cout << res << endl;
+    }
+    return 0;
+}
