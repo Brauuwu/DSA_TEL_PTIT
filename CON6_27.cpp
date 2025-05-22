1c33ad7d2c3eb6672fb666ed6c57ec2e38c7dec8
Brauuwu <survivalminecraft080@gmail.com>
2025-05-22 17:00:47 +0700

    Sắp xếp chèn

---
+++ CON6_27.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		int a[n];
+		for(int &x:a) cin >> x;
+		for(int i=0; i<n; i++){
+			int k = a[i];
+			int j = i-1;
+			while(j>=0 && a[j] > k){
+				a[j+1] = a[j];
+				j--;
+			}
+			a[j+1] = k;
+			cout << "Buoc " << i << ": ";
+			for(j=0; j<=i; j++){
+				cout << a[j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
